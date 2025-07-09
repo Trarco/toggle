edec192f1ffd727659867f8b36ba5c1add64c4e2
Trarco <trarco22@outlook.it>
2025-07-09 15:14:37 +0200

    Add IT

---
+++ lang/it/block_toggle.php
@@ -0,0 +1,11 @@
+<?php
+
+$string['blocktitle'] = 'Titolo del blocco';
+$string['blockcontent'] = 'Contenuto generale';
+$string['parttitle'] = 'Titolo della parte';
+$string['partsubtitle'] = 'Sottotitolo della parte';
+$string['partcontent'] = 'Contenuto della parte';
+$string['addpart'] = 'Aggiungi un\'altra parte della giornata';
+
+$string['defaultblocktitle'] = 'Titolo';
+$string['defaultblockcontent'] = 'Contenuto del blocco';
